c5d0af72a188b50c4c693c74371331034767ffd6
u7chan <t.unagami.dev@gmail.com>
2023-11-15 22:40:33 +0900

    feat: EnhancedTable example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Container, Typography } from '@mui/material'
 
+import EnhancedTable from './EnhancedTable'
+
 function App() {
   return (
     <Container>
       <Typography variant="h3">mui-table-example</Typography>
+      <EnhancedTable />
     </Container>
   )
 }
